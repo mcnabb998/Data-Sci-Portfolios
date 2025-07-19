1fb1f8876382284fd60c130d9cf4a701af68b87d
Matthew McNabb <mcnabb998@gmail.com>
2025-07-18 22:10:25 -0500

    fixed routing

--- docs/react-portfolio/src/App.js
+++ docs/react-portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './portfolio.css';
 import Nav from './components/Nav';
@@ -7,11 +8,18 @@ import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <Projects />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<><Hero /><Projects /></>} />
+          <Route path="/about" element={<div>About page coming soon.</div>} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<div>Contact page coming soon.</div>} />
+          {/* Add more routes for individual projects as needed */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
