0e2e86d9f2ec937158ee3c91c2b4642b41b976e7
Matthew McNabb <mcnabb998@gmail.com>
2025-07-18 22:08:52 -0500

    Fix contact page link and update info

--- docs/react-portfolio/src/components/Nav.js
+++ docs/react-portfolio/src/components/Nav.js
@@ -4,9 +4,9 @@ export default function Nav() {
   return (
     <nav>
       <a href="../../index.html">Home</a> |
-      <a href="../../about.md">About</a> |
+      <a href="../../about.html">About</a> |
       <a href="../../index.html#projects">Projects</a> |
-      <a href="../../contact.md">Contact</a>
+      <a href="../../contact.html">Contact</a>
     </nav>
   );
 }
